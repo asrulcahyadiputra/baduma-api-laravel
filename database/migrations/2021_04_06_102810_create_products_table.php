88439e0eb702672b333f6f4bc4fe631901b146d6
Asrul Cahyadi Putra <asrulcahyadiputra99@gmail.com>
2021-04-06 17:41:46 +0700

    Created products table migration updated

--- database/migrations/2021_04_06_102810_create_products_table.php
+++ database/migrations/2021_04_06_102810_create_products_table.php
@@ -15,6 +15,18 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('sku',20)->unique();
+            $table->string('name',50);
+            $table->text('description')->nullable();
+            $table->bigInteger('minimum_price');
+            $table->bigInteger('maximum_price');
+            $table->bigInteger('category_id');
+            $table->string('path');
+            $table->string('base_image');
+            $table->boolean('disc');
+            $table->float('disc_ammount')->default('0');
+            $table->date('disc_start')->nullable();
+            $table->date('disc_end')->nullable();
             $table->timestamps();
         });
     }
