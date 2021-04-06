c920bf46c6829fbd834e66761154751b7d964325
Asrul Cahyadi Putra <asrulcahyadiputra99@gmail.com>
2021-04-06 17:21:09 +0700

    Created product tags and product categorys table migration

---
+++ database/migrations/2021_04_06_101715_create_product_tags_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',30);
+            $table->string('path');
+            $table->string('image_name');
+            $table->text('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_tags');
+    }
+}
