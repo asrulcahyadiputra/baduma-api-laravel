be0573e52a14a5867ae32e731c797b0f3de4294d
Asrul Cahyadi Putra <asrulcahyadiputra99@gmail.com>
2021-04-06 18:06:09 +0700

    Created product brand table migration

---
+++ database/migrations/2021_04_06_105950_create_product_brands_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductBrandsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_brands', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',30);
+            $table->text('description')->nullable();
+            $table->string('path');
+            $table->string('image_name');
+            $table->date('since')->nullable();
+            $table->timestamps();
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_brands');
+    }
+}
