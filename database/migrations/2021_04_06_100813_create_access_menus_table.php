00c72cf93a91d4039f9f1b06128e46ceeb270717
Asrul Cahyadi Putra <asrulcahyadiputra99@gmail.com>
2021-04-06 17:10:08 +0700

    Created access menus table migration

---
+++ database/migrations/2021_04_06_100813_create_access_menus_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccessMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('access_menus', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('menu_id');
+            $table->bigInteger('role_id');
+            $table->timestamps();
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('access_menus');
+    }
+}
